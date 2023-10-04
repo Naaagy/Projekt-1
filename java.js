eda7246724f968043944c5e55ec2d389e1324383
Naaagy <145921671+Naaagy@users.noreply.github.com>
2023-10-04 15:02:13 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,44 @@
+const btnR = document.querySelector('.btn-right');
+const btnL = document.querySelector('.btn-left');
+const tracks = document.querySelector('.tracks');
+const tracksW = tracks.scrollWidth;
+
+btnR.addEventListener('click', _ => {
+  tracks.scrollBy({
+    left: tracksW / 2,
+    behavior: 'smooth'
+  });
+});
+
+btnL.addEventListener('click', _ => {
+  tracks.scrollBy({
+    left: -tracksW / 2,
+    behavior: 'smooth'
+  });
+});
+
+
+let observer = new IntersectionObserver(entries => {
+  if (!entries[0].isIntersecting) {
+    document.body.classList.add("reveal");
+  } else {
+    document.body.classList.remove("reveal");
+  }
+});
+observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
+
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+});
